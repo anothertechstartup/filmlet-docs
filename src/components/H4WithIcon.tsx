20ed0129e6aacc1a10200585c7cbd5e221f0168f
Thiemo Imgraben <thiemo.imgraben@mway.io>
2023-01-02 14:08:43 +0100

    init

---
+++ src/components/H4WithIcon.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+export default function H4WithIcon({children, iconName}) {
+  return (
+    <h4 style={{display:"flex", alignItems:"center", marginBottom:"0px"}}>
+      {children}
+      <img src={useBaseUrl(`/ico/${iconName}`)} height='26px' style={{marginLeft:'10px', }}/>
+    </h4>
+  );
+}
---
+++ src/theme/Icon/Arrow/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export default function IconArrow(props) {
+  return (
+    <svg width="20" height="20" aria-hidden="true" {...props}><path d="m24 40-2.1-2.15L34.25 25.5H8v-3h26.25L21.9 10.15 24 8l16 16Z"/></svg>
+  );
+}
